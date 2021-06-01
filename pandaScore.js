139d3c37c02f8a2563927f15f76f1e26c0df39dd
Emily Gleeson <engleeson@gmail.com>
2021-06-01 19:04:07 -0400

    added cors authentication link

--- pandaScore.js
+++ pandaScore.js
@@ -1,17 +1,16 @@
 
-var test="https://api.pandascore.co/codmw/matches?filter[id]=590448&token=_Sb6lpEcpdk-URomVTPLYWMJg3OTz9gQvy8sWyM3NcEhLMuyFyo"
+var test="https://cors-anywhere.herokuapp.com/https://api.pandascore.co/codmw/matches?filter[id]=590448&token=_Sb6lpEcpdk-URomVTPLYWMJg3OTz9gQvy8sWyM3NcEhLMuyFyo"
 
 var url='https://api.pandascore.co/lol/champions?token=_Sb6lpEcpdk-URomVTPLYWMJg3OTz9gQvy8sWyM3NcEhLMuyFyo'
 
-fetch(test, {
-    mode: "no-cors",
-   Authorization: 'Bearer' + '_Sb6lpEcpdk-URomVTPLYWMJg3OTz9gQvy8sWyM3NcEhLMuyFyo'
-})
+fetch(test)
   .then(function (response) {
       console.log(response)
-      console.log(response.text())
+return response.json();
 //line 13 throwing errors - "unexpected end of input"
-    return response.json();
+   // return response.json();
+  }).then(function(data){
+    console.log(data)
   })
 //   .then(function (data) {
 //     console.log(data);

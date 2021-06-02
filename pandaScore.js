8f1084942b69aab4e70e205766971ac67606bb83
patrick brodie <patrickxbrodie@hotmail.com>
2021-06-02 19:26:11 -0400

    my html work

--- pandaScore.js
+++ pandaScore.js
@@ -1,61 +1,54 @@
-//dumping info - COD only - players, matches, series, tournaments, leagues - can pare down as we continue
 
-var players = "https://cors-anywhere.herokuapp.com/https://api.pandascore.co/codmw/players?token=_Sb6lpEcpdk-URomVTPLYWMJg3OTz9gQvy8sWyM3NcEhLMuyFyo"
 
-var leagues = "https://cors-anywhere.herokuapp.com/https://api.pandascore.co/codmw/leagues?token=_Sb6lpEcpdk-URomVTPLYWMJg3OTz9gQvy8sWyM3NcEhLMuyFyo"
 
-var matches ="https://cors-anywhere.herokuapp.com/https://api.pandascore.co/codmw/matches?token=_Sb6lpEcpdk-URomVTPLYWMJg3OTz9gQvy8sWyM3NcEhLMuyFyo"
-
-var series = "https://cors-anywhere.herokuapp.com/https://api.pandascore.co/codmw/series?token=_Sb6lpEcpdk-URomVTPLYWMJg3OTz9gQvy8sWyM3NcEhLMuyFyo"
-
-var teams = "https://cors-anywhere.herokuapp.com/https://api.pandascore.co/codmw/teams?token=_Sb6lpEcpdk-URomVTPLYWMJg3OTz9gQvy8sWyM3NcEhLMuyFyo"
 
 var tournaments = "https://cors-anywhere.herokuapp.com/https://api.pandascore.co/codmw/tournaments?token=_Sb6lpEcpdk-URomVTPLYWMJg3OTz9gQvy8sWyM3NcEhLMuyFyo"
 
-fetch(players)
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  console.log(data);
-});
 
-fetch(leagues)
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  console.log(data);
-});
-
-fetch(matches)
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  console.log(data);
-});
-
-fetch(series)
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  console.log(data);
-});
-
-fetch(teams)
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  console.log(data);
-});
 
 fetch(tournaments)
 .then(function (response) {
   return response.json();
 })
 .then(function (data) {
-  console.log(data);
-});
+ 
+  var mostRecent =  document.getElementById("recent-tournament");
+  mostRecent.innerHTML = data[1].teams[2].name;
+
+  var teamList = document.getElementById("team-list");
+  teamList.textContent = "Teams";
+  data.forEach(item=>teamNames(data));
+
+
+
+
+  function teamNames (streams) {
+    var names = []
+console.log(streams)
+    for( var i = 0; i < streams.length; i++){
+      var teams = streams[i].teams
+      
+      
+      for( let team of teams){
+        // console.log("the ",team)
+        names.push(team.name)
+      }
+      
+    }
+    // console.log(names)
+    return names ;
+
+
+
+  }
+
+
+    // for( i = 0; i < data.length; i++){
+    //   var teams = data.teams[i]
+    // console.log("these are the teams, " + teams)
+    // var li = document.createElement("li");
+    // li.textContent = data[1].teams[i].name;
+    // teamList.appendChild(li);
+    // console.log(item)
+//   }}
+})
